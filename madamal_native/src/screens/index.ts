b07f8b5b05788bf22cc62f85ec4f58a8ae747637
galronen10 <galronen10@gmail.com>
2024-03-22 18:19:11 +0200

    add basic navigation

---
+++ madamal_native/src/screens/index.ts
@@ -0,0 +1,3 @@
+export {HomeScreen} from './home';
+export {UserProfileScreen} from './userProfile';
+export {UserReportsScreen} from './userReports';
--- madamal_native/src/screens/userReports/index.tsx
+++ madamal_native/src/screens/userReports/index.tsx
@@ -1,10 +1,10 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 
-export const HomeScreen = () => {
+export const UserReportsScreen = () => {
   return (
     <View>
-      <Text>HomeScreen</Text>
+      <Text>UserReportsScreen</Text>
     </View>
   );
 };

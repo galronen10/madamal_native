14786b9893e40ea9a31bf04611b2191ffeae9331
galronen10 <galronen10@gmail.com>
2024-03-30 17:49:30 +0300

    add user data display

--- madamal_native/src/screens/userProfile/index.tsx
+++ madamal_native/src/screens/userProfile/index.tsx
@@ -1,10 +1,46 @@
-import { View, Text } from 'react-native';
-import React, { FC } from 'react';
+import { View, Image, Text } from 'react-native';
+import React, { FC, useState } from 'react';
+import { userStub } from '@/constants/userStub';
+import { IBasicUserData } from '@/models/user';
+import { Button, TextInput } from 'react-native-paper';
+import { styles } from './styles';
+import { MadaMalBanner } from '@/components/common';
 
 export const UserProfileScreen: FC = () => {
+  const [userData, setUserData] = useState<IBasicUserData>(userStub);
+
+  const goToEdit = (): void => {};
   return (
-    <View>
-      <Text>UserProfile</Text>
+    <View style={styles.container}>
+      <MadaMalBanner />
+      <View style={styles.userData}>
+        <TextInput
+          style={styles.textDisplay}
+          label="אימייל"
+          editable={false}
+          value={userData.email}
+          textAlign="right"
+        />
+        <TextInput
+          style={styles.textDisplay}
+          label="שם מלא"
+          editable={false}
+          value={userData.fullName}
+          textAlign="right"
+        />
+
+        {userData.imageUrl && (
+          <Image
+            style={styles.userImage}
+            source={{ uri: userData.imageUrl }}
+            resizeMode="contain"
+          />
+        )}
+      </View>
+
+      <Button mode="contained" onPress={goToEdit} style={styles.button}>
+        <Text>ערוך פרופיל</Text>
+      </Button>
     </View>
   );
 };
--- madamal_native/src/screens/userProfile/styles.ts
+++ madamal_native/src/screens/userProfile/styles.ts
@@ -1,3 +1,25 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+export const styles = StyleSheet.create({
+  button: {
+    alignSelf: 'center',
+    marginBottom: 20,
+    width: '50%',
+  },
+  container: {
+    flex: 1,
+  },
+  textDisplay: {
+    marginBottom: 20,
+  },
+  userData: {
+    flex: 1,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  userImage: {
+    alignSelf: 'center',
+    height: 250,
+    width: 250,
+  },
+});

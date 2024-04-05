b83d3bce07256db14805bb824660b4814d8f3187
galronen10 <galronen10@gmail.com>
2024-04-05 15:59:38 +0300

    add store to project

---
+++ madamal_native/src/redux/user/index.ts
@@ -0,0 +1,51 @@
+import { StoreUser } from '@/models/user';
+import { createSelector, createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from '..';
+
+// Define a type for the slice state
+interface UserState {
+  userData: StoreUser;
+}
+
+// Define the initial state using that type
+const initialState: UserState = {
+  userData: { userId: '', email: '', fullName: '', imageUrl: '' },
+};
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {
+    updateUser: (state, action: PayloadAction<Partial<StoreUser>>) => {
+      return {
+        ...state,
+        userData: { ...state.userData, ...action.payload },
+      };
+    },
+    logout: () => {
+      return {
+        ...initialState,
+      };
+    },
+  },
+});
+
+export const { updateUser: updateUser, logout } = userSlice.actions;
+
+export const selectUser = (state: RootState): StoreUser => state.user.userData;
+
+export const selectUserId = createSelector(
+  selectUser,
+  (user: StoreUser): string => user.userId,
+);
+
+export const selectIsUserLoggedIn = createSelector(
+  selectUserId,
+  (userId: string): boolean => !!userId,
+);
+export const selectUserName = createSelector(
+  selectUser,
+  (user: StoreUser): string => user.fullName,
+);
+
+export default userSlice.reducer;
